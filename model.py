6ed4971d21afc0d331b222329d98a1982035f312
Ruichen Xu <bill.xu.github@gmail.com>
2026-08-16 05:32:42 -0400

    Step 17: elementwise_add

--- model.py
+++ model.py
@@ -150,8 +150,14 @@ def slice_subblock(arr, r0, r1, c0, c1):
     return arr[r0:r1, c0:c1]
     pass
 
-# Step 17 - elementwise_add (not yet solved)
-# TODO: implement
+# Step 17 - elementwise_add
+import numpy as np
+
+def elementwise_add(a, b):
+    """Return the elementwise sum of two same-shape arrays."""
+    # TODO: return a new array whose entries are the pairwise sums of a and b
+    return a + b
+    pass
 
 # Step 18 - elementwise_multiply (not yet solved)
 # TODO: implement
