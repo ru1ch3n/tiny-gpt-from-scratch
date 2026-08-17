fb4afec418589dde3c61df788ab1f9c8b8806b4a
Ruichen Xu <bill.xu.github@gmail.com>
2026-08-16 21:20:13 -0400

    Step 35: encode_corpus_to_int_array

--- model.py
+++ model.py
@@ -323,8 +323,12 @@ def read_text_file(text_blob):
       
     pass
 
-# Step 35 - encode_corpus_to_int_array (not yet solved)
-# TODO: implement
+# Step 35 - encode_corpus_to_int_array
+def encode_corpus_to_int_array(text, stoi):
+    """Convert the corpus string into a 1D NumPy int64 array of token ids."""
+    # TODO: map every character in text through stoi and return as a 1D int64 array
+    return np.array([ stoi[char] for char in text])
+    pass
 
 # Step 36 - pick_split_point (not yet solved)
 # TODO: implement
