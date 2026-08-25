45a34b5178e4b324ced64f53be9922ffd15e8c3e
Ruichen Xu <bill.xu.github@gmail.com>
2026-08-25 17:46:54 -0400

    Step 53: decode_generated_sequence

--- model.py
+++ model.py
@@ -489,8 +489,12 @@ def generate_sequence(p_matrix, start_id, length, rng):
     return next_id
     pass
 
-# Step 53 - decode_generated_sequence (not yet solved)
-# TODO: implement
+# Step 53 - decode_generated_sequence
+def decode_generated_sequence(ids, itos):
+    """Decode a generated 1D array/list of token ids into a string via itos."""
+    # TODO: turn ids into a readable string using itos
+    return "".join([itos[i] for i in ids])
+    pass
 
 # Step 54 - log_prob_of_pair (not yet solved)
 # TODO: implement
