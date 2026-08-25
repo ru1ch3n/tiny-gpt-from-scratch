753b2def9b1e4f39184d3e2eaba782a4e2b31051
Ruichen Xu <bill.xu.github@gmail.com>
2026-08-25 18:02:29 -0400

    Step 56: average_nll

--- model.py
+++ model.py
@@ -516,8 +516,11 @@ def sum_negative_log_probs(p_matrix, data):
     return res
     pass
 
-# Step 56 - average_nll (not yet solved)
-# TODO: implement
+# Step 56 - average_nll
+def average_nll(p_matrix, data):
+    # TODO: return mean negative log likelihood per bigram over consecutive pairs in data.
+    return sum_negative_log_probs(p_matrix, data)/(len(data)-1)
+    pass
 
 # Step 57 - initialize_w_random (not yet solved)
 # TODO: implement
