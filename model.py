5fa77e426be5cbf22927b19a2d6f499f46a1f8d2
Ruichen Xu <bill.xu.github@gmail.com>
2026-08-25 15:57:31 -0400

    Step 44: get_batch

--- model.py
+++ model.py
@@ -401,8 +401,15 @@ def stack_y_batch(data, offsets, block_size):
     
     pass
 
-# Step 44 - get_batch (not yet solved)
-# TODO: implement
+# Step 44 - get_batch
+def get_batch(data, block_size, batch_size, rng):
+    # TODO: package one training batch (X, Y) of shape (batch_size, block_size) from data using rng.
+    data_len = len(data)
+    offsets = sample_random_batch_offsets(data_len, block_size, batch_size, rng)
+    X = stack_x_batch(data, offsets, block_size) 
+    Y = stack_y_batch(data, offsets, block_size)
+    return X, Y
+    pass
 
 # Step 45 - allocate_count_matrix (not yet solved)
 # TODO: implement
