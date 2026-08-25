15e904a65d024a3f52c19edc46057aa2234a32ef
Ruichen Xu <bill.xu.github@gmail.com>
2026-08-25 18:44:16 -0400

    Step 60: forward_logits_onehot

--- model.py
+++ model.py
@@ -553,8 +553,11 @@ def one_hot_encode_batch(ids, vocab_size):
     return one_hot
     pass
 
-# Step 60 - forward_logits_onehot (not yet solved)
-# TODO: implement
+# Step 60 - forward_logits_onehot
+def forward_logits_onehot(onehot, w_matrix):
+    # TODO: compute logits for the neural bigram model as the matrix product of one-hot inputs and W.
+    return matmul(onehot, w_matrix)
+    pass
 
 # Step 61 - observe_lookup_equivalence (not yet solved)
 # TODO: implement
